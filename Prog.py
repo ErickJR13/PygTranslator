340de5e82865ef6e235f68790c3d5ee8c5963e4d
Erick Reyes <erickjeanreyes@Ericks-MBP.T-mobile.com>
2017-01-02 14:47:56 -0500

    Changed to function model instead of a main-if model

--- Prog.py
+++ Prog.py
@@ -9,59 +9,75 @@ print "Welcome to Pig Latin! I will help you changed your desired word into the 
 
 time.sleep(2)
 
-word = raw_input("Enter the word: ") #takes word from user
-
-pyg = 'ay' #This is the "ay" add-on
-pyg2 = 'way' #for words that start with a vowel, no rearrangenment is made and the word "way" is added in the end
-
-time.sleep(0.9)
-
-print "Hold on while I translate this for you."
-
-time.sleep(0.5)
-
-print "***********************************"
-
-time.sleep(0.5)
-
-if len(word) > 0:
-
-    print "You entered a valid word!"
-
-else:
-
-    print "You didn't enter a word! Close the program and open it again."
-
-
-time.sleep(0.5)
-
-
-print "***********************************"
-
-time.sleep(0.5)
+def translator(w):
 
 
 
-if  word.isalpha():
+    pyg = 'ay'  # This is the "ay" add-on
+    pyg2 = 'way'  # for words that start with a vowel, no rearrangenment is made and the word "way" is added in the end
 
-    print "The word doesn't contain a non-alphabetic character!"
+    time.sleep(0.9)
 
-else:
+    print "Hold on while I translate this for you."
 
-    print "This word contains a number or a symbol. Close the program and try again"
+    time.sleep(0.5)
+
+    print "***********************************"
+
+    time.sleep(0.5)
+
+    if len(w) > 0:
+
+        print "You entered a valid word!"
+
+    else:
+
+        print "You didn't enter a word! Close the program and open it again."
 
 
-print "***********************************"
+    time.sleep(0.5)
+
+
+    print "***********************************"
+
+    time.sleep(0.5)
+
+
+    if word.isalpha():
+
+        print "The word doesn't contain a non-alphabetic character!"
+
+    else:
+
+        print "This word contains a number or a symbol. Close the program and try again"
+
+
+    print "***********************************"
+
+
+    if w.startswith(("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")):
+
+        lowercasedword = w.lower()
+        translation = lowercasedword + pyg2
+
+    else:
+
+
+        lowercasedword = w.lower()
+        firstletter = lowercasedword[0]
+        translation = lowercasedword[1:] + firstletter + pyg
+
+
+    print "Your translated word is: "
+
+    time.sleep(1)
+
+    print translation
 
 
 
 
-lowercasedword = word.lower()
-firstletter = lowercasedword[0]
-translation = lowercasedword[1:] + firstletter + pyg
 
-print "Your translated word is: "
 
-time.sleep(1)
-
-print translation
+word = raw_input("Enter a word: ")
+translator(word)
